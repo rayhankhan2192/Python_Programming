6fa3dc0fe790bd2bcb5e49021701ab466a142517
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2023-09-06 22:27:25 +0600

    User input

---
+++ Basic Python/1UserInput.py
@@ -0,0 +1,9 @@
+name = input("What is your name: ")
+print("your name: "+name)
+
+num1 = input("\nEnter any number: ")
+print("You entered: "+num1)
+
+birthday = input("\nEnter your birthday: ")
+age = 2023 - int(birthday)
+print("Your age: ",age)
